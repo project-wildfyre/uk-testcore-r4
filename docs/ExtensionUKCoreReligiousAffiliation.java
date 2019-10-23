21c89018538f01e780d0e573452245552e614793
bushra <bushra.khatoon@nhs.net>
2019-10-23 10:05:33 +0100

    Committed docs

---
+++ docs/ExtensionUKCoreReligiousAffiliation.java
@@ -0,0 +1,7 @@
+package https://fhir.nhs.uk/R4/ImplementationGuide/nhsdigital-fhir-r4-core-demo;
+
+import org.hl7.fhir.r5.model.ProfilingWrapper;
+
+public class ExtensionUKCoreReligiousAffiliation {
+
+}
