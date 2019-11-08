14bb7f15983699a15e7e49b9a6c84be14bbb6b3a
IOPS-DEV <interoperabilityteam@nhs.net>
2019-11-08 11:32:40 +0000

    Publish

--- docs/UKCorePatient.java
+++ docs/UKCorePatient.java
@@ -1,4 +1,4 @@
-package https://fhir.nhs.uk/R4/ImplementationGuide/nhsdigital-fhir-r4-core-demo;
+package https://fhir.nhs.uk/R4/ImplementationGuide/nhsdigital-fhir-r4-core-prototype;
 
 import org.hl7.fhir.r5.model.ProfilingWrapper;
 
